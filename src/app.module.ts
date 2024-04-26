926105b24fa079adee2894572a12c52a152f892b
Edward-Brock <1416477069@qq.com>
2024-04-26 12:51:28 +0800

    build: 安装 @nestjs/config 依赖； chore: 通过环境变量修改启动监听路径；

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    UsersModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+  const http_url = configService.get('SERVER_URL');
+  const http_port = configService.get('SERVER_PORT');
+  await app.listen(http_port);
+  console.log(`
+  ============================================================
+  🎉 HTTP SERVICE STARTED: ${http_url}:${http_port}
+  ============================================================
+  `);
 }
 bootstrap();
