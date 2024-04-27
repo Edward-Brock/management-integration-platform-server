cccffc4fce24802b889e5f8c1d4085da34c0edbd
Edward-Brock <1416477069@qq.com>
2024-04-27 21:08:32 +0800

    build: 安装 class-transformer、class-validator 依赖； feat: 为 DTO 引入验证修饰器；

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   const configService: ConfigService = app.get<ConfigService>(ConfigService);
   const server_env = configService.get('SERVER_ENV');
   const http_url = configService.get('SERVER_URL');
--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,12 +1,34 @@
+import {
+  IsDate,
+  IsEmail,
+  IsMobilePhone,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
   id: number;
   name?: string;
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+  @IsEmail()
   email?: string;
+  @IsMobilePhone()
   mobile?: string;
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(36)
   password: string;
+  @IsString()
   avatar?: string;
+  @IsString()
   language: string;
+  @IsDate()
   createdAt: Date;
+  @IsDate()
   updatedAt: Date;
 }
